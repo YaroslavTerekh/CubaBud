c377fa47976d7ca9af0b4a1b23404b93eb102976
Yaroslav Terekh <yarolslavterekh@gmail.com>
2022-10-28 13:37:57 +0300

    Added basic models

---
+++ CubaBud.Domain/Models/CallBack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubaBud.Domain.Models
+{
+    public class CallBack : BaseModel
+    {
+        public string PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string Wishes { get; set; }
+    }
+}
